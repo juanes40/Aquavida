815eefc295e4607df54372474f837dfabf66fdbd
Alejandro Ortega <alortega1012@outlook.com>
2023-10-02 10:19:23 -0500

    02OC

---
+++ PHP/connection.php
@@ -0,0 +1,20 @@
+<?php
+// Conexión a la base de datos (reemplaza con tus propios datos)
+function connection() {
+    // Configuración de conexión a la base de datos
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "aqua_vida";
+
+    // Crear y verificar la conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("La conexión a la base de datos falló: " . $conn->connect_error);
+    }
+
+    return $conn; // Devuelve la conexión
+}
+
+?>
---
+++ PHP/insert_user.php
@@ -0,0 +1,22 @@
+<?php
+include("connection.php");
+$conn = connection();
+
+$id = $_POST['identificacion'];
+$name = $_POST['nombres'];
+$lastname = $_POST['apellidos'];
+$username = $_POST['usuario'];
+$password = $_POST['contraseña'];
+
+
+$sql = "INSERT INTO users VALUES('$id','$name','$lastname','$username','$password')";
+$query = mysqli_query($conn, $sql);
+
+if($query){
+    
+    Header("Location: ../html/crearUsuario.html");
+}else{
+
+}
+
+?>
