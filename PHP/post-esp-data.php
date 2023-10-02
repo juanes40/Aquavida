48c2c3a025eeab7c013c6c7eb51e6877eed193e4
Alejandro-OB <alortega1012@outlook.com>
2023-10-02 17:15:10 -0500

    Corregir convenciones de final de línea en Sensores.ino

--- PHP/post-esp-data.php
+++ PHP/post-esp-data.php
@@ -1,20 +1,10 @@
 <?php
 
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
 
 $servername = "localhost";
 
 // REPLACE with your Database name
-$dbname = "Aqua_vida";
+$dbname = "aqua_vida";
 // REPLACE with Database user
 $username = "root";
 // REPLACE with Database user password
@@ -24,8 +14,7 @@ $password = "";
 // If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
 
-$api_key= $sensor1 = $location = $value1 =$sensor2 = $value2 = $tiempo = $alarma = "";
-//$action = $sensor1 = $location = $value1 =$sensor2 = $value2 = $tiempo = $alarma = "";
+$api_key= $sensor1 = $location = $value1 =$sensor2 = $value2 = $sensor3 = $value3 = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
@@ -35,8 +24,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $value1 = test_input($_POST["value1"]);
         $sensor2 = test_input($_POST["sensor2"]);
         $value2 = test_input($_POST["value2"]);
-        $tiempo = test_input($_POST["tiempo"]);
-        $alarma = test_input($_POST["alarma"]);
+        $sensor3 = test_input($_POST["sensor3"]);
+        $value3 = test_input($_POST["value3"]);
+        
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
         // Check connection
@@ -44,8 +34,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Connection failed: " . $conn->connect_error);
         } 
         
-        $sql = "INSERT INTO SensorData (sensor1, location, value1, sensor2, value2, tiempo , alarma)
-        VALUES ('" . $sensor1 . "', '" . $location . "', '" . $value1 . "', '" . $sensor2 . "', '" . $value2 . "', '" . $tiempo ."', '" . $alarma . "')";
+        $sql = "INSERT INTO SensorData (sensor1, location, value1, sensor2, value2, sensor3, value3)
+        VALUES ('" . $sensor1 . "', '" . $location . "', '" . $value1 . "','" . $sensor2 . "', '" . $value2 . "', '" . $sensor3 . "', '" . $value3 . "')";
         
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
@@ -61,48 +51,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
 }
-else if ($_SERVER["REQUEST_METHOD"] == "GET"){
-    $api_key = test_input($_GET["api_key"]);
-    if($api_key == $api_key_value){
-        $varX = test_input($_GET["var"]);
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $sql = "SELECT * FROM sensordata ORDER BY id DESC LIMIT 1";
-        if ($result = $conn->query($sql)) {
-            $row = mysqli_fetch_row($result);
-            echo $row[8];
-            //echo "tiempo";
-        }
-        else {
-            echo false;
-        }
-        $conn->close();
-    } else {
-        echo "Wrong API key provided";
-    }
-}
 else {
-    echo "No data posted with HTTP POST/GET.";
+    echo "No data posted with HTTP POST.";
 }
-/*
-function getAllOutputStates() {
-    global $servername, $username, $password, $dbname;
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    $sql = "SELECT id, sensor1, location, value1, sensor2, value2, reading_time, alarma, tiempo FROM sensordata ORDER BY id DESC LIMIT 1";
-    if ($result = $conn->query($sql)) {
-        return $result;
-    }
-    else {
-        return false;
-    }
-    $conn->close();
-}*/
 
 function test_input($data) {
     $data = trim($data);
