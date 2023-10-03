6f27842fee64446f1a90c4e2a07f39badc96cf86
Alejandro-OB <alortega1012@outlook.com>
2023-10-02 23:57:36 -0500

    consultar datos y editar

---
+++ PHP/consultar_datos_users.php
@@ -0,0 +1,50 @@
+<?php
+// Realiza la conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "aqua_vida";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("La conexión a la base de datos falló: " . $conn->connect_error);
+}
+
+// Obtén el ID del usuario enviado desde el formulario
+if(isset($_POST["idUsuario"])) {
+    $idUsuario = $_POST["idUsuario"];
+    
+    // Realiza la consulta SQL para obtener los datos del usuario
+    $sql = "SELECT id, name, lastname, username, password FROM users WHERE id = $idUsuario";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // El usuario fue encontrado, obtén los datos
+        $row = $result->fetch_assoc();
+        $nombres = $row["name"];
+        $apellidos = $row["lastname"];
+        $usuario = $row["username"];
+        $contraseña = $row["password"];
+        
+        // Crea un arreglo asociativo con los datos del usuario
+        $usuarioEncontrado = array(
+            "nombres" => $nombres,
+            "apellidos" => $apellidos,
+            "usuario" => $usuario,
+            "contraseña" => $contraseña
+        );
+
+        // Devuelve los datos en formato JSON
+        echo json_encode($usuarioEncontrado);
+    } else {
+        // El usuario no fue encontrado
+        echo "Usuario no encontrado";
+    }
+} else {
+    echo "ID del usuario no proporcionado";
+}
+
+// Cierra la conexión a la base de datos
+$conn->close();
+?>
--- PHP/update_user.php
+++ PHP/update_user.php
@@ -1,21 +1,33 @@
 <?php
+// Conexión a la base de datos (reemplaza los datos con los tuyos)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "aqua_vida";
 
-include("connection.php");
-$con = connection();
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-$id = $_POST['identificacion'];
-$name = $_POST['nombres'];
-$lastname = $_POST['apellidos'];
-$username = $_POST['usuario'];
-$password = $_POST['contraseña'];
-
-$sql="UPDATE users VALUES('$id','$name','$lastname','$username','$password')";
-$query = mysqli_query($con, $sql);
-
-if($query){
-    Header("Location: ../html/editarUsuario.html");;
-}else{
-
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
 }
 
+// Obtener datos del formulario
+$idUsuario = $_POST["idUsuario"];
+$nombres = $_POST["nombres"];
+$apellidos = $_POST["apellidos"];
+$identificacion = $_POST["identificacion"];
+$usuario = $_POST["usuario"];
+$contraseña = $_POST["contraseña"];
+
+// Actualizar datos en la base de datos (esto es solo un ejemplo, debes ajustarlo según tu estructura de base de datos)
+$sql = "UPDATE users SET nombres='$nombres', apellidos='$apellidos', identificacion='$identificacion', usuario='$usuario', contraseña='$contraseña' WHERE id='$idUsuario'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Los datos se actualizaron correctamente.";
+} else {
+    echo "Error al actualizar los datos: " . $conn->error;
+}
+
+$conn->close();
 ?>
