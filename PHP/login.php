4c12ee0dbac34ab77b5e07b0de4752cea98a728e
Alejandro-OB <alortega1012@outlook.com>
2023-10-14 15:25:27 -0500

    añadí el identificador al login para que haga la redirección correcta

--- PHP/login.php
+++ PHP/login.php
@@ -6,18 +6,27 @@ $conn = connection();
 $usuario = $_POST["usuario"];
 $contraseña = $_POST["contraseña"];
 
-// Consulta SQL para obtener el hash de la contraseña almacenada en la base de datos
-$sql = "SELECT password FROM users WHERE username='$usuario'";
+// Consulta SQL para obtener el hash de la contraseña y el valor de "identifier" almacenado en la base de datos
+$sql = "SELECT password, identifier FROM users WHERE username='$usuario'";
 $result = $conn->query($sql);
 
 if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
     $hashFromDatabase = $row['password'];
+    $identifier = $row['identifier'];
 
     // Verifica si la contraseña proporcionada coincide con el hash almacenado
     if (password_verify($contraseña, $hashFromDatabase)) {
-        // Inicio de sesión exitoso, redirige al usuario a la página de inicio
-        header("Location: ../HTML/adminInicial.html");
+        // Comprueba el valor de "identifier" y redirige al usuario según su valor
+        if ($identifier == 0) {
+            header("Location: ../HTML/adminInicial.html");
+        } elseif ($identifier == 1) {
+            header("Location: ../HTML/usuarioFull.html");
+        } else {
+            // Valor de "identifier" no reconocido, puedes manejarlo de otra manera si es necesario
+            echo "<script>alert('Valor de identifier no reconocido.');</script>";
+            echo "<script>window.location.href='../HTML/login.html';</script>";
+        }
     } else {
         echo "<script>alert('Usuario o contraseña inválidos. Por favor, inténtelo de nuevo.');</script>";
         echo "<script>window.location.href='../HTML/login.html';</script>";
@@ -30,3 +39,4 @@ if ($result->num_rows == 1) {
 // Cierra la conexión a la base de datos
 $conn->close();
 ?>
+
